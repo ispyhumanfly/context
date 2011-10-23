e1f1fbf6ab8f2ee6b51b5e178766c7700befeb57
Dan Stephenson <ispyhumanfly@gmail.com>
2011-10-22 21:19:43 -0400

    The control panel is kinda working now.

---
+++ script/After5-Model-1.0a-SQLite.sql
@@ -0,0 +1,47 @@
+-- 
+-- Created by SQL::Translator::Producer::SQLite
+-- Created on Sat Oct 22 01:51:49 2011
+-- 
+
+BEGIN TRANSACTION;
+
+--
+-- Table: ads
+--
+DROP TABLE ads;
+
+CREATE TABLE ads (
+  id INTEGER PRIMARY KEY NOT NULL,
+  title varchar(100) NOT NULL,
+  price int(10) NOT NULL,
+  datetime datetime(50) NOT NULL
+);
+
+--
+-- Table: content
+--
+DROP TABLE content;
+
+CREATE TABLE content (
+  id INTEGER PRIMARY KEY NOT NULL,
+  datetime datetime(50) NOT NULL,
+  tags varchar(100) NOT NULL,
+  cities varchar(100) NOT NULL,
+  title varchar(100) NOT NULL,
+  body varchar(5000) NOT NULL
+);
+
+--
+-- Table: settings
+--
+DROP TABLE settings;
+
+CREATE TABLE settings (
+  id INTEGER PRIMARY KEY NOT NULL,
+  admin varchar(32) NOT NULL,
+  password varchar(1000) NOT NULL,
+  about varchar(10000) NOT NULL,
+  datetime datetime(50) NOT NULL
+);
+
+COMMIT;
