10508bb65b2facf909d7bad875f6112ee4f9329d
Dan Stephenson <ispyhumanfly@gmail.com>
2011-10-25 18:24:06 -0400

    Now some of the pages render the content in different ways.

--- script/After5-Model-1.0a-SQLite.sql
+++ script/After5-Model-1.0a-SQLite.sql
@@ -1,6 +1,6 @@
 -- 
 -- Created by SQL::Translator::Producer::SQLite
--- Created on Sun Oct 23 19:21:42 2011
+-- Created on Mon Oct 24 20:38:49 2011
 -- 
 
 BEGIN TRANSACTION;
@@ -25,10 +25,15 @@ DROP TABLE content;
 CREATE TABLE content (
   id INTEGER PRIMARY KEY NOT NULL,
   datetime datetime(50) NOT NULL,
+  type varchar(100) NOT NULL,
   tags varchar(100) NOT NULL,
   cities varchar(100) NOT NULL,
   title varchar(100) NOT NULL,
-  body varchar(5000) NOT NULL
+  body varchar(5000) NOT NULL,
+  start_time datetime(50),
+  end_time datetime(50),
+  lat float(100),
+  lng float(100)
 );
 
 --
