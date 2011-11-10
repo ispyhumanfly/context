2e3f5f5b10c30406cfc689e83c51fdde160f4bb1
Dan Stephenson <ispyhumanfly@gmail.com>
2011-11-10 11:07:57 -0500

    After my recent meeting with Aubreii, I've made some updates. After5 is really starting to take shape.

--- script/After5-Model-1.0a-SQLite.sql
+++ script/After5-Model-1.0a-SQLite.sql
@@ -1,6 +1,6 @@
 -- 
 -- Created by SQL::Translator::Producer::SQLite
--- Created on Mon Oct 31 02:38:08 2011
+-- Created on Thu Nov 10 00:16:13 2011
 -- 
 
 BEGIN TRANSACTION;
@@ -13,7 +13,8 @@ DROP TABLE ads;
 CREATE TABLE ads (
   id INTEGER PRIMARY KEY NOT NULL,
   title varchar(100) NOT NULL,
-  price int(10) NOT NULL,
+  pages varchar(50) NOT NULL,
+  photo varchar(500) NOT NULL,
   datetime datetime(50) NOT NULL
 );
 
@@ -25,15 +26,20 @@ DROP TABLE content;
 CREATE TABLE content (
   id INTEGER PRIMARY KEY NOT NULL,
   datetime datetime(50) NOT NULL,
-  type varchar(100) NOT NULL,
+  types varchar(100) NOT NULL,
   tags varchar(100) NOT NULL,
   cities varchar(100) NOT NULL,
+  interests varchar(100) NOT NULL,
+  pages varchar(50),
+  photos varchar(10000),
   title varchar(100) NOT NULL,
   body varchar(5000) NOT NULL,
-  start_time datetime(50),
-  end_time datetime(50),
-  lat float(100),
-  lng float(100)
+  start_datetime datetime(50),
+  stop_datetime datetime(50),
+  address varchar(100),
+  lat float(50),
+  lng float(50),
+  position int(10)
 );
 
 --
