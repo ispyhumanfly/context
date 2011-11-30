fde4761340b46dafcc1d2fcc520ad77c1d37ebcb
Dan Stephenson <ispyhumanfly@gmail.com>
2011-11-29 19:39:41 -0500

    Further refinements. Enhancements to the CPanel, and the addition of the hiremyway iframe for the Work page.

--- script/After5-Model-1.0a-SQLite.sql
+++ script/After5-Model-1.0a-SQLite.sql
@@ -1,22 +1,9 @@
 -- 
 -- Created by SQL::Translator::Producer::SQLite
--- Created on Thu Nov 10 00:16:13 2011
+-- Created on Mon Nov 28 21:00:48 2011
 -- 
 
 BEGIN TRANSACTION;
-
---
--- Table: ads
---
-DROP TABLE ads;
-
-CREATE TABLE ads (
-  id INTEGER PRIMARY KEY NOT NULL,
-  title varchar(100) NOT NULL,
-  pages varchar(50) NOT NULL,
-  photo varchar(500) NOT NULL,
-  datetime datetime(50) NOT NULL
-);
 
 --
 -- Table: content
@@ -26,17 +13,18 @@ DROP TABLE content;
 CREATE TABLE content (
   id INTEGER PRIMARY KEY NOT NULL,
   datetime datetime(50) NOT NULL,
-  types varchar(100) NOT NULL,
-  tags varchar(100) NOT NULL,
-  cities varchar(100) NOT NULL,
-  interests varchar(100) NOT NULL,
+  types varchar(100),
+  tags varchar(100),
+  cities varchar(100),
+  interests varchar(100),
   pages varchar(50),
   photos varchar(10000),
   title varchar(100) NOT NULL,
-  body varchar(5000) NOT NULL,
-  start_datetime datetime(50),
-  stop_datetime datetime(50),
+  body varchar(5000),
+  start_datetime datetime(100),
+  stop_datetime datetime(100),
   address varchar(100),
+  author varchar(100),
   lat float(50),
   lng float(50),
   position int(10)
