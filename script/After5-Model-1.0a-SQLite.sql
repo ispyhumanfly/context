01e97b743902f84fe066328c2a773fd534e1e4f1
Dan Stephenson <ispyhumanfly@gmail.com>
2011-12-07 18:15:37 -0500

    Many adjustments and enhancements based on Aubreii's most recent review of the interface.

--- script/After5-Model-1.0a-SQLite.sql
+++ script/After5-Model-1.0a-SQLite.sql
@@ -1,6 +1,6 @@
 -- 
 -- Created by SQL::Translator::Producer::SQLite
--- Created on Mon Nov 28 21:00:48 2011
+-- Created on Tue Dec  6 05:38:16 2011
 -- 
 
 BEGIN TRANSACTION;
@@ -13,18 +13,18 @@ DROP TABLE content;
 CREATE TABLE content (
   id INTEGER PRIMARY KEY NOT NULL,
   datetime datetime(50) NOT NULL,
+  author varchar(100) NOT NULL,
+  title varchar(100) NOT NULL,
   types varchar(100),
   tags varchar(100),
   cities varchar(100),
   interests varchar(100),
   pages varchar(50),
   photos varchar(10000),
-  title varchar(100) NOT NULL,
   body varchar(5000),
   start_datetime datetime(100),
   stop_datetime datetime(100),
   address varchar(100),
-  author varchar(100),
   lat float(50),
   lng float(50),
   position int(10)
